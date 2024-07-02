352282098e3038828674122abb363e52aaf1d879
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-02 10:56:24 +0900

    add : 포켓몬 데이터 가져오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <>처음</>;
+  const [pokemons, setPokemons] = useState<Pokemon[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const response = await axios.get("http://localhost:3000/api/pokemons");
+      console.log(response.data);
+      setPokemons(response.data);
+    };
+    fetchData();
+  }, []);
+
+  if (!pokemons) return <div>...로딩중</div>;
+
+  return (
+    <div>
+      <h1>포켓몬 목록</h1>
+      <ul className="grid grid-cols-4">
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id} className="">
+            <img
+              src={pokemon.sprites.front_default}
+              alt={pokemon.korean_name}
+            />
+            <h1>{pokemon.korean_name}</h1>
+            <p>도감번호: {pokemon.id}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
